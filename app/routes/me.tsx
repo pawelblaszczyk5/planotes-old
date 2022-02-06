3178fbecf7d9f16c591f1617bbb4ad9d57755304
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2022-02-06 22:36:04 +0100

    Hide id from user

--- app/routes/me.tsx
+++ app/routes/me.tsx
@@ -5,20 +5,15 @@ import { json, useLoaderData } from 'remix';
 import { auth } from '~/lib/utils';
 
 export const loader: LoaderFunction = async ({ request }) => {
-  const user = await auth.isAuthenticated(request, { failureRedirect: '/login' });
+  const { email } = await auth.isAuthenticated(request, { failureRedirect: '/login' });
 
-  return json(user);
+  return json({ email });
 };
 
 const Screen = () => {
   const user = useLoaderData<User>();
 
-  return (
-    <h1>
-      {user.email}
-      {user.id}
-    </h1>
-  );
+  return <h1>{user.email}</h1>;
 };
 
 export default Screen;
