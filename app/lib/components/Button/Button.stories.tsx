41b04b99be70e995c59fea3bb1818a0e7dee98c3
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2022-02-13 20:11:40 +0100

    Button component

--- app/lib/components/Button/Button.stories.tsx
+++ app/lib/components/Button/Button.stories.tsx
@@ -1,12 +1,61 @@
 import type { ComponentStory, ComponentMeta } from '@storybook/react';
+import type { ComponentProps } from 'react';
 
 import { Button } from '~/lib/components/Button';
 
 export default {
-  title: 'Button',
+  title: 'Interactive/Button',
   component: Button,
+  argTypes: {
+    onClick: {
+      control: false,
+    },
+    children: {
+      control: {
+        type: 'text',
+      },
+      defaultValue: 'Click me',
+    },
+    color: {
+      defaultValue: 'primary',
+    },
+    size: {
+      defaultValue: 'normal',
+    },
+  },
+  parameters: {
+    controls: {
+      include: ['color', 'size', 'onClick', 'children'],
+    },
+  },
 } as ComponentMeta<typeof Button>;
 
-const Template: ComponentStory<typeof Button> = () => <Button />;
+const Template: ComponentStory<typeof Button> = ({ children, ...restProps }: ComponentProps<typeof Button>) => (
+  <Button {...restProps}>{children}</Button>
+);
 
-export const Test = Template.bind({});
+export const Primary = Template.bind({});
+
+Primary.args = {
+  color: 'primary',
+};
+
+export const Secondary = Template.bind({});
+
+Secondary.args = {
+  color: 'secondary',
+};
+
+export const Small = Template.bind({});
+
+Small.args = {
+  size: 'small',
+};
+
+export const Normal = Template.bind({});
+
+export const Large = Template.bind({});
+
+Large.args = {
+  size: 'large',
+};
--- app/lib/components/Button/Button.tsx
+++ app/lib/components/Button/Button.tsx
@@ -1,1 +1,44 @@
-export const Button = () => <button className="border-2">Test</button>;
+import type { ReactNode } from 'react';
+import type { HTMLMotionProps } from 'framer-motion';
+
+import { motion } from 'framer-motion';
+import clsx from 'clsx';
+
+type ButtonColor = 'primary' | 'secondary';
+type ButtonSize = 'small' | 'normal' | 'large';
+
+interface ButtonProps extends HTMLMotionProps<'button'> {
+  color?: ButtonColor;
+  size?: ButtonSize;
+  children: ReactNode;
+}
+
+export const Button = ({ color = 'primary', size = 'normal', children, ...restProps }: ButtonProps) => (
+  <motion.button
+    whileTap={{ scale: 0.95 }}
+    className={clsx(
+      'group inline-grid place-items-center overflow-hidden rounded-lg bg-gradient-to-br text-zinc-800 ring-violet-600/50 ring-offset-zinc-50 hover:text-zinc-50 focus:outline-none dark:text-zinc-50	dark:ring-violet-400/75 dark:ring-offset-zinc-800',
+      {
+        'from-emerald-400 to-cyan-600': color === 'primary',
+        'from-rose-400 to-pink-600': color === 'secondary',
+        'p-0.5 text-sm font-medium ring-offset-1 focus-visible:ring-2': size === 'small',
+        'p-0.5 font-semibold ring-offset-2 focus-visible:ring-2': size === 'normal',
+        'p-1 text-2xl font-bold ring-offset-2 focus-visible:ring': size === 'large',
+      },
+    )}
+    {...restProps}
+  >
+    <span
+      className={clsx(
+        'rounded-md bg-zinc-50 transition duration-75 ease-in group-hover:bg-opacity-0 dark:bg-zinc-800',
+        {
+          'rounded-md px-3 py-1.5': size === 'small',
+          'rounded-md px-5 py-2.5': size === 'normal',
+          'rounded px-6 py-3': size === 'large',
+        },
+      )}
+    >
+      {children}
+    </span>
+  </motion.button>
+);
