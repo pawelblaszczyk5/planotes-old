2196fa2679813d4d697dacbea51d39f6ebd910fa
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2022-02-18 23:28:10 +0100

    Copy fonts files after install too

--- scripts/copyFonts.js
+++ scripts/copyFonts.js
@@ -3,6 +3,7 @@ const fs = require('fs/promises');
 const path = require('path');
 
 const fontSizes = [300, 400, 500, 600, 700, 800];
+const regex = /fonts\/((\w|-|\.)+)/gm;
 
 (async () => {
   const data = (
@@ -12,6 +13,14 @@ const fontSizes = [300, 400, 500, 600, 700, 800];
   )
     .reduce((prev, curr) => prev + curr.toString(), '')
     .replaceAll('./files', '/fonts');
+
+  const fontMatches = [...data.matchAll(regex)];
+
+  await Promise.all(
+    fontMatches.map(([, fontFile]) =>
+      fs.copyFile(path.join('node_modules/@fontsource/manrope/files', fontFile), path.join('public/fonts', fontFile)),
+    ),
+  );
 
   await fs.writeFile(path.join('app', 'fonts.css'), data);
 })();
