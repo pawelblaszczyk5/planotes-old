936bf0d51ad29eccd60ae3fc2cc8740b5995f34b
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2022-02-20 19:52:57 +0100

    Add protection to routes

--- app/routes/app/home.tsx
+++ app/routes/app/home.tsx
@@ -1,3 +1,13 @@
+import type { LoaderFunction } from 'remix';
+
+import { isAuthenticated } from '~/lib/utils';
+
+export const loader: LoaderFunction = async ({ request }) => {
+  await isAuthenticated(request);
+
+  return null;
+};
+
 const Screen = () => <h1>Home</h1>;
 
 export default Screen;
--- app/routes/app/index.tsx
+++ app/routes/app/index.tsx
@@ -1,7 +1,10 @@
 import type { LoaderFunction } from 'remix';
 
 import { redirect } from 'remix';
+import { isAuthenticated } from '~/lib/utils';
 
-export const loader: LoaderFunction = () => {
+export const loader: LoaderFunction = async ({ request }) => {
+  await isAuthenticated(request);
+
   return redirect('/app/home');
 };
