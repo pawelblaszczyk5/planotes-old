101e0473391425bb5490ea3e25e18e953714510b
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2022-06-08 19:11:39 +0200

    refactor: code style

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -7,27 +7,28 @@ import { I18nextProvider, initReactI18next } from 'react-i18next';
 import { getInitialNamespaces } from 'remix-i18next';
 import { i18nConfig } from '~/lib/constants';
 
-// eslint-disable-next-line import/no-named-as-default-member
-i18next
-  .use(initReactI18next)
-  .use(LanguageDetector)
-  .use(Backend)
-  .init({
-    ...i18nConfig,
-    ns: getInitialNamespaces(),
-    backend: {
-      loadPath: '/locales/{{lng}}/{{ns}}.json',
-    },
-    detection: {
-      order: ['htmlTag'],
-      caches: [],
-    },
-  })
-  .then(() => {
-    return hydrateRoot(
-      document,
-      <I18nextProvider i18n={i18next}>
-        <RemixBrowser />
-      </I18nextProvider>,
-    );
-  });
+(async () => {
+  // eslint-disable-next-line import/no-named-as-default-member
+  await i18next
+    .use(initReactI18next)
+    .use(LanguageDetector)
+    .use(Backend)
+    .init({
+      ...i18nConfig,
+      ns: getInitialNamespaces(),
+      backend: {
+        loadPath: '/locales/{{lng}}/{{ns}}.json',
+      },
+      detection: {
+        order: ['htmlTag'],
+        caches: [],
+      },
+    });
+
+  return hydrateRoot(
+    document,
+    <I18nextProvider i18n={i18next}>
+      <RemixBrowser />
+    </I18nextProvider>,
+  );
+})();
